cbc18a4092c91a9834dd4bd13dbf4b6faaa272f3
Olajide Ojo <olajide@Olajides-Laptop.local>
2023-04-21 14:58:51 +0100

    add latest file change for BDD for lumin skin

---
+++ cypress/e2e/Luminskin/PurchaseProduct.feature
@@ -0,0 +1,15 @@
+
+Feature: Purchase a skincare product
+
+    A customer should be to make a succesful product purchase
+
+    Scenario: Buy a skincare product on luminskin website
+        Given I visit the page
+        Then I select a product of choice and make payment
+
+
+
+
+
+
+
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,7 +20,7 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require('@badeball/cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
